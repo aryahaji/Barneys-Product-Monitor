90d041b517eef348336071a04766b669964064d2
Arya Hajikandi <aryahaji@buffalo.edu>
2019-01-06 01:48:11 -0500

    added integration with Twilio API to notify user via text when product is in stock

--- monitor.py
+++ monitor.py
@@ -2,6 +2,11 @@
 
 import requests
 from bs4 import BeautifulSoup
+from twilio.rest import Client
+
+account_sid = 'your account sid'
+auth_token = 'your auth token'
+client = Client(account_sid, auth_token)
 
 headers = {
     'User-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.77 Safari/537.36'
@@ -12,10 +17,12 @@ page = requests.get("https://www.barneys.com/product/canada-goose-macculloch-dow
 soup = BeautifulSoup(page, "lxml")
 element = soup.find("a", {"data-skuid":"00505057258287"})
 
-availability = False
-
 if element.get("data-availabilitystatus") == "1000":
-    availability = True
-    print('In stock')
+    message = client.messages \
+                .create(
+                     body="The product is currently in stock!",
+                     from_='+twilio phone number you buy',
+                     to='+your phone number'
+                 )
 else:
-    print('out of stock')
+    pass
