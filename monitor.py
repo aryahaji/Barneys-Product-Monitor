01991332f1ab057d3c0c137bbdf7b6a00e4342df
Arya Hajikandi <aryahaji@buffalo.edu>
2019-01-06 14:01:52 -0500

    Revert "General improvement to performance and general usability"

--- monitor.py
+++ monitor.py
@@ -9,21 +9,22 @@ from twilio.rest import Client
 
 load_dotenv()
 
-product_url = ""
-product_sku = ""
-
-account_sid = os.getenv("account_sid")
-auth_token = os.getenv("auth_token")
-client = Client(account_sid, auth_token)
+while True:
+    account_sid = os.getenv("account_sid")
+    auth_token = os.getenv("auth_token")
+    client = Client(account_sid, auth_token)
 
     headers = {
         "User-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.77 Safari/537.36"
     }
 
-while True:
-    page = requests.get(product_url,headers=headers,).text
+    page = requests.get(
+        "https://www.barneys.com/product/canada-goose-macculloch-down-parka-505725823.html",
+        headers=headers,
+    ).text
+
     soup = BeautifulSoup(page, "lxml")
-    element = soup.find("a", {"data-skuid": product_sku})
+    element = soup.find("a", {"data-skuid": "00505057258287"})
     quantity = element.get("data-onhand-quantity")
 
     if element.get("data-availabilitystatus") == "1000":
